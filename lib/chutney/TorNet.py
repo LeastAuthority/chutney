32a61c63182acfe29b4249c9303da57304dcf854
Jim Newsome <jnewsome@torproject.org>
2024-11-07 19:36:48 -0600

    Move getenv helpers into Util

--- lib/chutney/TorNet.py
+++ lib/chutney/TorNet.py
@@ -17,7 +17,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from pathlib import Path
-from typing import List, Optional, TypeVar, Callable, Any, Iterable, overload, Union
+from typing import List, Optional, TypeVar, Any, Iterable, Union, Callable
 
 import errno
 import importlib
@@ -33,6 +33,7 @@ import base64
 
 from chutney.Debug import debug_flag, debug
 from chutney.Templating import Environ
+from chutney.Util import getenv_int, getenv_bool
 from chutney.network_tests import NetworkTestFailure
 from collections.abc import Collection
 from importlib.abc import Traversable
@@ -128,96 +129,6 @@ class ChutneyInternalError(ChutneyError):
 
 
 T = TypeVar("T")
-
-
-@overload
-def getenv_type(
-    env_var: str,
-    default: None,
-    type_: Callable[[str], T],
-    type_name: Optional[str] = None,
-) -> Optional[T]: ...
-
-
-@overload
-def getenv_type(
-    env_var: str, default: T, type_: Callable[[str], T], type_name: Optional[str] = None
-) -> T: ...
-
-
-def getenv_type(
-    env_var: str,
-    default: Optional[T],
-    type_: Callable[[str], T],
-    type_name: Optional[str] = None,
-) -> Optional[T]:
-    """
-    Return the value of the environment variable 'envar' as type_,
-    or 'default' if no such variable exists.
-
-    Raise ValueError using type_name if the environment variable is set,
-    but type_() raises a ValueError on its value. (If type_name is None
-    or empty, the ValueError uses type_'s string representation instead.)
-    """
-    strval = os.environ.get(env_var)
-    if strval is None:
-        return default
-    try:
-        return type_(strval)
-    except ValueError:
-        if not type_name:
-            type_name = str(type_)
-        raise ValueError(
-            (
-                "Invalid value for environment variable '{}': "
-                "expected {}, but got '{}'"
-            ).format(env_var, type_name, strval)
-        )
-
-
-@overload
-def getenv_int(env_var: str, default: None) -> Optional[int]: ...
-
-
-@overload
-def getenv_int(env_var: str, default: int) -> int: ...
-
-
-def getenv_int(env_var: str, default: Optional[int]) -> Optional[int]:
-    """
-    Return the value of the environment variable 'envar' as an int,
-    or 'default' if no such variable exists.
-
-    Raise ValueError if the environment variable is set, but is not an int.
-    """
-    return getenv_type(env_var, default, int, type_name="an int")
-
-
-def getenv_bool(env_var: str, default: bool) -> bool:
-    """
-    Return the value of the environment variable 'envar' as a bool,
-    or 'default' if no such variable exists.
-
-    Unlike bool(), converts 0, "False", and "No" to False.
-
-    Raise ValueError if the environment variable is set, but is not a bool.
-    """
-    # TODO: consider simplifying this and:
-    # * rejecting ints other than 0 or 1
-    # * accepting 'yes' for True
-    try:
-        # Handle integer values
-        return bool(getenv_int(env_var, int(default)))
-    except ValueError:
-        # Handle values that the user probably expects to be False
-        strval = os.environ.get(env_var)
-        # If the env var weren't set, the int path above would have succeeded,
-        # returning the default.
-        assert strval is not None
-        if strval.lower() in ["false", "no"]:
-            return False
-        else:
-            return getenv_type(env_var, default, bool, type_name="a bool")
 
 
 def mkdir_p(*d: Union[str, Path], mode: int = 448) -> None:
--- lib/chutney/Util.py
+++ lib/chutney/Util.py
@@ -3,8 +3,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import os
+
 from collections.abc import Iterable
-from typing import Callable, TypeVar, Any
+from typing import Callable, TypeVar, Any, Optional, overload
 from typing_extensions import ParamSpec
 
 P = ParamSpec("P")
@@ -28,3 +30,93 @@ def memoized(fn: Callable[P, T]) -> Callable[P, T]:
         return result
 
     return memoized_fn
+
+
+@overload
+def getenv_type(
+    env_var: str,
+    default: None,
+    type_: Callable[[str], T],
+    type_name: Optional[str] = None,
+) -> Optional[T]: ...
+
+
+@overload
+def getenv_type(
+    env_var: str, default: T, type_: Callable[[str], T], type_name: Optional[str] = None
+) -> T: ...
+
+
+def getenv_type(
+    env_var: str,
+    default: Optional[T],
+    type_: Callable[[str], T],
+    type_name: Optional[str] = None,
+) -> Optional[T]:
+    """
+    Return the value of the environment variable 'envar' as type_,
+    or 'default' if no such variable exists.
+
+    Raise ValueError using type_name if the environment variable is set,
+    but type_() raises a ValueError on its value. (If type_name is None
+    or empty, the ValueError uses type_'s string representation instead.)
+    """
+    strval = os.environ.get(env_var)
+    if strval is None:
+        return default
+    try:
+        return type_(strval)
+    except ValueError:
+        if not type_name:
+            type_name = str(type_)
+        raise ValueError(
+            (
+                "Invalid value for environment variable '{}': "
+                "expected {}, but got '{}'"
+            ).format(env_var, type_name, strval)
+        )
+
+
+@overload
+def getenv_int(env_var: str, default: None) -> Optional[int]: ...
+
+
+@overload
+def getenv_int(env_var: str, default: int) -> int: ...
+
+
+def getenv_int(env_var: str, default: Optional[int]) -> Optional[int]:
+    """
+    Return the value of the environment variable 'envar' as an int,
+    or 'default' if no such variable exists.
+
+    Raise ValueError if the environment variable is set, but is not an int.
+    """
+    return getenv_type(env_var, default, int, type_name="an int")
+
+
+def getenv_bool(env_var: str, default: bool) -> bool:
+    """
+    Return the value of the environment variable 'envar' as a bool,
+    or 'default' if no such variable exists.
+
+    Unlike bool(), converts 0, "False", and "No" to False.
+
+    Raise ValueError if the environment variable is set, but is not a bool.
+    """
+    # TODO: consider simplifying this and:
+    # * rejecting ints other than 0 or 1
+    # * accepting 'yes' for True
+    try:
+        # Handle integer values
+        return bool(getenv_int(env_var, int(default)))
+    except ValueError:
+        # Handle values that the user probably expects to be False
+        strval = os.environ.get(env_var)
+        # If the env var weren't set, the int path above would have succeeded,
+        # returning the default.
+        assert strval is not None
+        if strval.lower() in ["false", "no"]:
+            return False
+        else:
+            return getenv_type(env_var, default, bool, type_name="a bool")
