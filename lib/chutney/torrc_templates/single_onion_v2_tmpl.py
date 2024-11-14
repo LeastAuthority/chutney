31375f18ca6629de1040b27fd15f9682f5d9c493
Jim Newsome <jnewsome@torproject.org>
2024-11-14 11:23:58 -0600

    Delete torrc templates using hs-v2

--- lib/chutney/torrc_templates/single_onion_v2_tmpl.py
+++
@@ -1,9 +0,0 @@
-from chutney.TorNet import Node
-from . import single_onion_common_i
-
-
-def format(n: Node) -> str:
-    return f"""\
-{single_onion_common_i.format(n)}
-HiddenServiceVersion 2
-"""
