e6b1659e7233de464a2286439e3470ae2349884b
Jim Newsome <jnewsome@torproject.org>
2024-11-20 15:09:00 -0600

    Merge authority.i into common.i

--- lib/chutney/torrc_templates/authority_tmpl.py
+++ lib/chutney/torrc_templates/authority_tmpl.py
@@ -1,9 +1,10 @@
+import re
+
 from chutney.TorNet import Node
-from . import relay_non_exit_tmpl, authority_i
+from . import relay_non_exit_tmpl
 
 
 def format(n: Node) -> str:
-    return f"""\
-{relay_non_exit_tmpl.format(n)}
-{authority_i.format(n)}
-"""
+    res = relay_non_exit_tmpl.format(n)
+    assert re.search(r"AuthoritativeDirectory 1", res), res
+    return res
--- lib/chutney/torrc_templates/common_i.py
+++ lib/chutney/torrc_templates/common_i.py
@@ -1,4 +1,4 @@
-from chutney.TorNet import Node, ChutneyError
+from chutney.TorNet import Node, ChutneyError, V3_AUTH_VOTING_INTERVAL
 
 
 def format(n: Node) -> str:
@@ -152,6 +152,43 @@ ExitPolicy accept6 private:*
 # 3. Finally, reject all IPv6 addresses which haven't been permitted
 # ------------------------------------------------------------------
 ExitPolicy reject6 *:*
+"""
+    if n._config.authority and not n._config.bridgeauthority:
+        res += f"""
+AuthoritativeDirectory 1
+V3AuthoritativeDirectory 1
+ContactInfo auth{n.nodenum}@test.test
+
+# Disable authority to relay/bridge reachability checks
+# These checks happen every half hour, even in testing networks
+# As of tor 0.4.3, there is no way to speed up these checks
+AssumeReachable 1
+
+# Speed up the consensus cycle as fast as it will go.
+# If clock desynchronisation is an issue, increase these voting times.
+
+# V3AuthVotingInterval and TestingV3AuthInitialVotingInterval can be:
+#   10, 12, 15, 18, 20, ...
+# TestingV3AuthInitialVotingInterval can also be:
+#    5, 6, 8, 9
+# They both need to evenly divide 24 hours.
+
+# Initial Vote + Initial Dist must be less than Initial Interval
+#
+# Mixed 0.3.3 and 0.3.4 networks are unstable, due to timing changes.
+# When all 0.3.3 and earlier versions are obsolete, we may be able to revert to
+# TestingV3AuthInitialVotingInterval 5
+TestingV3AuthInitialVotingInterval 20
+TestingV3AuthInitialVoteDelay 4
+TestingV3AuthInitialDistDelay 4
+# Vote + Dist must be less than Interval/2, because when there's no consensus,
+# tor uses Interval/2 as the voting interval
+#
+V3AuthVotingInterval {V3_AUTH_VOTING_INTERVAL}
+V3AuthVoteDelay 4
+V3AuthDistDelay 4
+
+ConsensusParams cc_alg=2
 """
 
     return res
