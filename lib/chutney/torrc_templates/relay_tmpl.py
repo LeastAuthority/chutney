ba7d6edb68575b3dad3a28ea875cba131688eaf7
Jim Newsome <jnewsome@torproject.org>
2024-11-20 11:07:19 -0600

    Merge exit-v4.i into common.i

--- lib/chutney/torrc_templates/relay_tmpl.py
+++ lib/chutney/torrc_templates/relay_tmpl.py
@@ -1,16 +1,13 @@
+import re
+
 from chutney.TorNet import Node
-from . import relay_non_exit_tmpl, exit_v4_i
-
-
-def format(n: Node) -> str:
-    return f"""\
-{relay_non_exit_tmpl.format(n)}
+from . import relay_non_exit_tmpl
 
 # This file is named "relay.tmpl" for compatibility with previous
 # chutney versions
 
-# An exit relay that can exit to IPv4 localhost
-# (newer versions of tor need this to be explicitly configured)
 
-{exit_v4_i.format(n)}
-"""
+def format(n: Node) -> str:
+    res = relay_non_exit_tmpl.format(n)
+    assert re.search(r"^ExitPolicy accept 127.0.0.0/8:\*", res, flags=re.MULTILINE), res
+    return res
--- lib/chutney/torrc_templates/relay_v6_tmpl.py
+++ lib/chutney/torrc_templates/relay_v6_tmpl.py
@@ -1,10 +1,7 @@
+import re
+
 from chutney.TorNet import Node
-from . import exit_v4_i, exit_v6_i, relay_non_exit_tmpl
-
-
-def format(n: Node) -> str:
-    return f"""\
-{relay_non_exit_tmpl.format(n)}
+from . import exit_v6_i, relay_non_exit_tmpl
 
 # This file is named "relay.tmpl" for compatibility with previous
 # chutney versions
@@ -12,6 +9,11 @@ def format(n: Node) -> str:
 # An exit relay that can exit to IPv4 & IPv6 localhost
 # (newer versions of tor need this to be explicitly configured)
 
-{exit_v4_i.format(n)}
+
+def format(n: Node) -> str:
+    res = f"""\
+{relay_non_exit_tmpl.format(n)}
 {exit_v6_i.format(n)}
 """
+    assert re.search(r"^ExitPolicy accept 127.0.0.0/8:*", res, flags=re.MULTILINE), res
+    return res
