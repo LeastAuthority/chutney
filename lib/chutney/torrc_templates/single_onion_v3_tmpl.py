64f7898506dce71997726ccae0f2301a03e88eb1
Jim Newsome <jnewsome@torproject.org>
2024-11-21 10:59:50 -0600

    Simplify single_onion_v3_tmpl
    The "HiddenServiceVersion" directive is already generated in `common_i`,
    so no need to do it again here.

--- lib/chutney/torrc_templates/single_onion_v3_tmpl.py
+++ lib/chutney/torrc_templates/single_onion_v3_tmpl.py
@@ -5,9 +5,7 @@ from . import common_i
 
 
 def format(n: Node) -> str:
-    res = f"""\
-{common_i.format(n)}
-HiddenServiceVersion 3
-"""
+    res = common_i.format(n)
     assert re.search(r"^HiddenServiceSingleHopMode 1", res, flags=re.MULTILINE), res
+    assert re.search(r"^HiddenServiceVersion 3", res, flags=re.MULTILINE), res
     return res
