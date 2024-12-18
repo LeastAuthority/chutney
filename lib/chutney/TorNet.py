fd7102ae3f0c0b6d7cd00a89ba39d74dca700d9d
Jim Newsome <jnewsome@torproject.org>
2024-12-18 13:13:57 -0600

    Inline legacy hs-v3-only-v6.tmpl wrapper

--- lib/chutney/TorNet.py
+++ lib/chutney/TorNet.py
@@ -3113,9 +3113,6 @@ def runConfigFile(verb: str, data: str) -> Optional[bool]:
                 ConnectionPadding 0
                 """
             )
-        elif torrc == "hs-v3-only-v6.tmpl":
-            kwargs["ip"] = None
-            kwargs["use_microdescriptors"] = False
         elif torrc == "relay-MAB.tmpl":
             # TODO: If we want to keep this, consider porting
             # to individual options.
