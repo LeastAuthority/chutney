7f073c31119a9db9ee4d39e0fa4e68891a4bb959
Jim Newsome <jnewsome@torproject.org>
2025-04-30 21:43:58 -0500

    LocalArtiNodeBuilder: use tomli_w to build arti config
    This is a little nicer to read and maintain, and heads off any subtle
    toml escaping/encoding issues.

--- lib/chutney/arti/builder.py
+++ lib/chutney/arti/builder.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import os
-import textwrap
+import tomli_w
 
 from typing_extensions import override
 from typing import Optional
@@ -35,92 +35,74 @@ class LocalArtiNodeBuilder(TorNet.NodeBuilder):
             raise ChutneyInternalError("Arti bridge unimplemented")
         if self._node._config.pt_bridge:
             raise ChutneyInternalError("Arti pt_bridge unimplemented")
-        arti_fallback_lines = []
-        arti_auth_lines = []
-        for auth in net.authorities:
-            if not auth.alt_dir_auth:
-                # We only configure dir auths, not bridge auths.
-                # TODO: configure bridge auths too, once arti supports them.
-                continue
-
-            addrs = '"%s:%s"' % (auth.ipv4, auth.orport)
-            if auth.ipv6.is_some():
-                addrs += ', "%s:%s"' % (
-                    auth.ipv6.unwrap(),
-                    auth.orport,
-                )
-            arti_fallback_lines.append(
-                "    {"
-                + f'rsa_identity = "{auth.fingerprint.replace(" ", "")}"'
-                + f', ed_identity = "{auth.fingerprint_ed25519}"'
-                + f", orports = [{addrs}]"
-                + "},\n"
-            )
-            arti_auth_lines.append(
-                "    {"
-                + f'name = "{auth.nick}"'
-                + f', v3ident = "{auth.v3id}"'
-                + "},\n"
-            )
-        res = textwrap.dedent(
-            f"""
-            [storage]
-            cache_dir = "{self._node.dir}/cache"
-            state_dir = "{self._node.dir}/state"
-
-            [path_rules]
-            # These values disable enforce_distance entirely; we can replace them
-            # with something like Tor's "EnforceDistinctSubnets 0" if Arti ever
-            # implements it.
-            ipv4_subnet_family_prefix = 33
-            ipv6_subnet_family_prefix = 129
-
-            [address_filter]
-            # Allow the client to accept requests to connect to e.g. 127.0.0.1
-            allow_local_addrs = true
-            """
-        )
-        res += textwrap.dedent(
-            """
-            [tor_network]
-            fallback_caches = [
-            """
-        )
-        res += "".join(arti_fallback_lines)
-        res += "]\n"
-        res += "authorities = [\n"
-        res += "".join(arti_auth_lines)
-        res += "]\n"
-
-        if self._node._config.bridgeclient:
-            res += textwrap.dedent(
-                """
-                [bridges]
-                enabled = "auto"
-                bridges = '''
-                """
-            )
-            for bd in net.bridges:
-                bridgeline: str
-                if bd.pt_transport.is_some():
-                    bridgeline = "{transport} {ip}:{port} {fp} {pt_extra}\n".format(
-                        transport=bd.pt_transport.unwrap(),
-                        ip=bd.ipaddr,
-                        port=bd.port,
-                        fp=bd.fingerprint,
-                        pt_extra=bd.pt_extra.unwrap_or_raise(
-                            ChutneyError("Missing pt_extra")
+        config = {
+            "storage": {
+                "cache_dir": str(self._node.dir.joinpath("cache")),
+                "state_dir": str(self._node.dir.joinpath("state")),
+            },
+            "path_rules": {
+                # These values disable enforce_distance entirely; we can replace them
+                # with something like Tor's "EnforceDistinctSubnets 0" if Arti ever
+                # implements it.
+                "ipv4_subnet_family_prefix": 33,
+                "ipv6_subnet_family_prefix": 129,
+            },
+            "address_filter": {
+                # Allow the client to accept requests to connect to e.g. 127.0.0.1
+                "allow_local_addrs": True
+            },
+            "tor_network": {
+                "fallback_caches": [
+                    {
+                        "rsa_identity": auth.fingerprint.replace(" ", ""),
+                        "ed_identity": auth.fingerprint_ed25519,
+                        "orports": (
+                            [f"{auth.ipv4}:{auth.orport}"]
+                            + (
+                                [f"{auth.ipv6.unwrap()}:{auth.orport}"]
+                                if auth.ipv6.is_some()
+                                else []
+                            )
                         ),
+                    }
+                    for auth in net.authorities
+                    if auth.alt_dir_auth
+                ],
+                "authorities": [
+                    {
+                        "name": auth.nick,
+                        "v3ident": auth.v3id,
+                    }
+                    for auth in net.authorities
+                    if auth.alt_dir_auth
+                ],
+            },
+        }
+        if self._node._config.bridgeclient:
+            config["bridges"] = {
+                "enabled": "auto",
+                "bridges": [
+                    (
+                        "{transport} {ip}:{port} {fp} {pt_extra}".format(
+                            transport=bd.pt_transport.unwrap(),
+                            ip=bd.ipaddr,
+                            port=bd.port,
+                            fp=bd.fingerprint,
+                            pt_extra=bd.pt_extra.unwrap_or_raise(
+                                ChutneyError("Missing pt_extra")
+                            ),
+                        )
+                        if bd.pt_transport.is_some()
+                        else "{ip}:{port} {fp}".format(
+                            ip=bd.ipaddr,
+                            port=bd.port,
+                            fp=bd.fingerprint,
+                        )
                     )
-                else:
-                    bridgeline = "{ip}:{port} {fp}\n".format(
-                        ip=bd.ipaddr,
-                        port=bd.port,
-                        fp=bd.fingerprint,
-                    )
-                res += bridgeline
-            res += "'''\n"
-        return res
+                    for bd in net.bridges
+                ],
+            }
+        return tomli_w.dumps(config)
 
     @override
     def checkConfig(self, net: TorNet.Network) -> None:
